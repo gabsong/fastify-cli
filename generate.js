ea5eda222a672491a02bdeb53aafc51b39974465
Gabriel Song <gabriel@deliverr.com>
2021-02-09 22:55:44 +0100

    Add missing keyword 'run' to npm command

--- generate.js
+++ generate.js
@@ -68,7 +68,7 @@ const typescriptTemplate = {
     log('info', `project ${pkg.name} generated successfully`)
     log('debug', `run '${chalk.bold('npm install')}' to install the dependencies`)
     log('debug', `run '${chalk.bold('npm start')}' to start the application`)
-    log('debug', `run '${chalk.bold('npm build:ts')}' to compile the typescript application`)
+    log('debug', `run '${chalk.bold('npm run build:ts')}' to compile the typescript application`)
     log('debug', `run '${chalk.bold('npm run dev')}' to start the application with pino-colada pretty logging (not suitable for production)`)
     log('debug', `run '${chalk.bold('npm test')}' to execute the unit tests`)
   }
